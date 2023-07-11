c53876fc1a4b8e2405bd788ce777be37056c52c9
malekazzouz <malekazzouz21@gmail.com>
2023-07-11 11:02:57 +0200

    My Django Girls app, first commit

---
+++ blog/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.contrib.gis.db import models
+
+
+from django.contrib.gis.db import models
+
+class Polygone(models.Model):
+    nom = models.CharField(max_length=100)
+    coordonnees = models.PolygonField()
+
+    def __str__(self):
+        return self.nom
+
+
